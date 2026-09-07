b6b59f03895217a631b911d391acbdac3b26abfd
Felipe M. Vieira <fmv1992@gmail.com>
2026-09-07 16:19:19 -0300

    updated data

--- test/test_one.adb
+++ test/test_one.adb
@@ -1,4 +1,3 @@
--- test_math.adb
 with AUnit.Assertions; use AUnit.Assertions;
 with Math_Core;
 
@@ -11,14 +10,30 @@ package body Test_Math is
 
    procedure Register_Tests (T : in out Test_Case) is
    begin
-      -- Map the string name to the actual procedure
-      Register_Routine (T, Test_Addition'Access, "Test Basic Addition");
+      Register_Routine (T, Test_Addition_Basic'Access, "Test Basic Addition");
+      Register_Routine (T, Test_Addition_Negative'Access, "Test Negative Addition");
+      Register_Routine (T, Test_Subtraction_Basic'Access, "Test Basic Subtraction");
+      Register_Routine (T, Test_Subtraction_Negative'Access, "Test Negative Subtraction");
    end Register_Tests;
 
-   procedure Test_Addition (T : in out AUnit.Test_Cases.Test_Case'Class) is
+   procedure Test_Addition_Basic (T : in out AUnit.Test_Cases.Test_Case'Class) is
    begin
-      -- AUnit assertion
-      Assert (Math_Core.Add (2, 2) = 4, "Addition failed!");
-   end Test_Addition;
+      Assert (Math_Core.Add (2, 2) = 4, "Basic addition failed!");
+   end Test_Addition_Basic;
+
+   procedure Test_Addition_Negative (T : in out AUnit.Test_Cases.Test_Case'Class) is
+   begin
+      Assert (Math_Core.Add (-5, 3) = -2, "Negative addition failed!");
+   end Test_Addition_Negative;
+
+   procedure Test_Subtraction_Basic (T : in out AUnit.Test_Cases.Test_Case'Class) is
+   begin
+      Assert (Math_Core.Subtract (5, 3) = 2, "Basic subtraction failed!");
+   end Test_Subtraction_Basic;
+
+   procedure Test_Subtraction_Negative (T : in out AUnit.Test_Cases.Test_Case'Class) is
+   begin
+      Assert (Math_Core.Subtract (-2, -3) = 1, "Negative subtraction failed!");
+   end Test_Subtraction_Negative;
 
 end Test_Math;
--- test/test_one.ads
+++ test/test_one.ads
@@ -1,15 +1,17 @@
--- test_math.ads
 with AUnit.Test_Cases;
 
 package Test_Math is
    type Test_Case is new AUnit.Test_Cases.Test_Case with null record;
 
-   -- Register the test name
+   -- Register the test name.
    function Name (T : Test_Case) return AUnit.Message_String;
 
-   -- Register the actual test procedures to run
+   -- Register the actual test procedures to run.
    procedure Register_Tests (T : in out Test_Case);
 
-   -- Individual test routines
+   -- Individual test routines.
+   procedure Test_Addition (T : in out AUnit.Test_Cases.Test_Case'Class);
+
+   -- Individual test routines.
    procedure Test_Addition (T : in out AUnit.Test_Cases.Test_Case'Class);
 end Test_Math;
