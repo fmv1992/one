379be8856a5d4c56bdcd1da56e19460dfbb9f17a
Felipe M. Vieira <fmv1992@gmail.com>
2026-09-07 15:35:58 -0300

    updated data

--- src/one.adb
+++ src/one.adb
@@ -1,4 +1,6 @@
-procedure One is
+with Ada.Text_IO;
+
+procedure Verify_Embedded_Bin is
 begin
-   null;
-end One;
+   Ada.Text_IO.Put_Line ("Hello from static Ada!");
+end Verify_Embedded_Bin;
--- src/verify_embedded_bin.adb
+++
@@ -1,6 +0,0 @@
-with Ada.Text_IO;
-
-procedure Verify_Embedded_Bin is
-begin
-   Ada.Text_IO.Put_Line ("Hello from static Ada!");
-end Verify_Embedded_Bin;
